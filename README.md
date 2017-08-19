b8dfcc1a807c73e29dae2fe2a7e26c5067429263
Andy Gimblett <andy@barefootcode.com>
2017-08-19 23:06:23 +0100

    Add some documentation and a licence

---
+++ README.md
@@ -0,0 +1,7 @@
+# Andy's very mini virtualenvwrapper-ish zsh plugin
+
+This is a super-lightweight sort-of-clone of [virtualenvwrapper](https://virtualenvwrapper.readthedocs.io/); it pretty much expects you to be using [pyenv](https://github.com/pyenv/pyenv) (though you don't *have* to), and because it's based on the [`venv` module](https://docs.python.org/3/library/venv.html), (creation) only works for python >= 3.3.
+
+Anyway, see the comments in [`venv-lite.zsh`](venv-lite.zsh) for all you need to know.
+
+Andy Gimblett, andy@barefootcode.com, 2017
--- venv-lite.zsh
+++ venv-lite.zsh
@@ -1,26 +1,62 @@
+# Andy's very mini virtualenvwrapper-ish zsh plugin
 
-# My mini virtualenvwrapper plugin
+# This is a simple, lightweight zsh plugin providing these three
+# commands, with this syntax and no other options:
+#
+# mkvirtualenv <venv-name>
+# workon <venv-name>
+# rmvirtualenv <venv-name>
+#
+# mkvirtualenv creates venvs in ~/.virtualenv; when a venv is created,
+# it is automatically activated too.
+#
+# workon and rmvirtualenv respectively activate and delete venvs in
+# ~/.virtualenv; tab completion works for these commands, based on the
+# directories in ~/.virtualenv
+#
+# mkvirtualenv uses whatever python is active, so use pyenv if you
+# want to choose your python version.
+#
+# mkvirtualenv creates venvs using python's stdlib's venv module; as
+# such, it only works for python versions >= 3.3.
+#
+# To create a venv for an earlier version of python (assuming pyenv
+# and its virtualenv support are installed) just create the venv
+# using, e.g.:
+#
+#     $ virtualenv -ppython2.7 ~/.virtualenvs/<name of venv>
+#
+# after which you can use this plugin's workon and rmvirtualenv on
+# that venv as usual.
+#
+# That's it!
+
 
 _venvs_path() {
     echo ~/.virtualenvs
 }
 
+
 _venv_path() {
     echo "$(_venvs_path)/$1"
 }
 
-rmvirtualenv () {
-    rm -rvf $(_venv_path $1)
-}
 
 mkvirtualenv () {
     python -m venv $(_venv_path $1)
     workon $1
 }
 
+
 workon () {
     source $(_venv_path $1)/bin/activate
 }
+
+
+rmvirtualenv () {
+    rm -rvf $(_venv_path $1)
+}
+
 
 _venv_completion () {
     _path_files -/ -W "$(_venvs_path)"
